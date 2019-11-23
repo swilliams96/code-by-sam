76ed1e7646dfaa49379bb860e5dd0577237f737a
Sam W <swilliams96@outlook.com>
2019-11-23 22:45:27 +0000

    Implement tag details page

--- src/app/components/tag-details-page/tag-details-page.component.ts
+++ src/app/components/tag-details-page/tag-details-page.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Project } from 'src/app/models/project.model';
+import { Subscription } from 'rxjs';
+import { ScrollService } from 'src/app/services/scroll.service';
 
 @Component({
   selector: 'app-tag-details-page',
   templateUrl: './tag-details-page.component.html',
   styleUrls: ['./tag-details-page.component.scss']
 })
-export class TagDetailsPageComponent implements OnInit {
+export class TagDetailsPageComponent implements OnInit, OnDestroy {
+  tag: string;
+  projects: Project[];
+  private _subscription: Subscription;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, public scrollService: ScrollService) {}
 
   ngOnInit() {
+    this._subscription = this.route.paramMap.subscribe(map => {
+      this.tag = map.get('tag');
+      this.projects = window.projects.filter(
+        project => project.tags && project.tags.map(tag => tag.toUpperCase()).includes(this.tag.toUpperCase())
+      );
+    });
   }
 
+  ngOnDestroy() {
+    if (this._subscription) {
+      this._subscription.unsubscribe();
+    }
+  }
 }
