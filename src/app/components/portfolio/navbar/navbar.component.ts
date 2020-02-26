a9399ab2ab8aae162f57733149b0400e51fe0272
Sam W <swilliams96@outlook.com>
2020-02-26 21:02:44 +0000

    Add contact modal

--- src/app/components/portfolio/navbar/navbar.component.ts
+++ src/app/components/portfolio/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ScrollService } from 'src/app/services/scroll.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-navbar',
@@ -11,7 +12,15 @@ export class NavbarComponent implements OnInit {
   @Input() projectsSectionId = 'projects';
   isCollapsed = true;
 
-  constructor(public scrollService: ScrollService) {}
+  constructor(public scrollService: ScrollService, private modalService: NgbModal) {}
 
-  ngOnInit() {}
+  ngOnInit() { }
+
+  open(content) {
+    this.modalService.open(content, { centered: true });
+  }
+
+  sendMessage() {
+    // send an email to me and upload something to firestore to show on the dashboard
+  }
 }
